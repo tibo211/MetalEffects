4c46a54bdfc93b9817577a9eafa7629c5b756e02
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2023-05-26 19:03:39 +0200

    Add SwiftUI view -> CGImage conversion

--- Sources/MetalEffects/MetalEffectsView.swift
+++ Sources/MetalEffects/MetalEffectsView.swift
@@ -7,14 +7,37 @@
 
 import SwiftUI
 
-public struct MetalEffectView: View {
+public struct MetalEffectView<Content: View>: View {
+    @ViewBuilder public let content: () -> Content
+    
+    @State private var image: CGImage?
+    
     public var body: some View {
-        Text("Hello, World!")
+        Group {
+            content()
+                .onAppear {
+                    createImage()
+                }
+        }
+    }
+    
+    @MainActor
+    private func createImage() {
+        let renderer = ImageRenderer(content: content())
+        renderer.scale = 3
+        image = renderer.cgImage
     }
 }
 
 struct MetalEffectsView_Previews: PreviewProvider {
     static var previews: some View {
-        MetalEffectView()
+        MetalEffectView {
+            Text("Rendered in metal")
+                .font(.title.bold())
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue.gradient)
+                .cornerRadius(12)
+        }
     }
 }
