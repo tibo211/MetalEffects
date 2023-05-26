66f52edf106debb6efe36dede09a80afe1071a3a
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2023-05-26 18:52:16 +0200

    Add MetalEffectView

---
+++ Sources/MetalEffects/MetalEffectsView.swift
@@ -0,0 +1,20 @@
+//
+//  MetalEffectView.swift
+//  
+//
+//  Created by Tibor Felföldy on 2023-05-26.
+//
+
+import SwiftUI
+
+public struct MetalEffectView: View {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MetalEffectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MetalEffectView()
+    }
+}
